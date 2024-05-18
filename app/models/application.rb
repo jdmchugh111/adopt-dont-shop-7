7f471a20a2c84d42fe808cd7b0216909c7a1f026
Zach Bergman <bzach634@gmail.com>
2024-05-18 07:53:07 -0600

    feat: Add validations to Application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,8 @@
 class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
 end
