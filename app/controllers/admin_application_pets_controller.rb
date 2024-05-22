7e0ba2216ce154bcb4684a30ef9516cc4dcd6459
James McHugh <jdmchugh111@gmail.com>
2024-05-21 21:19:45 -0400

    Wrote new migrations, refactored controller, and added to view - all tests passing

--- app/controllers/admin_application_pets_controller.rb
+++ app/controllers/admin_application_pets_controller.rb
@@ -2,7 +2,7 @@ class AdminApplicationPetsController < ApplicationController
   def update
     application_pet = ApplicationPet.find(params[:id])
     
-    application_pet.application.update(status: params[:status])
+    application_pet.update(pet_status: params[:status])
 
     redirect_to "/admin/applications/#{params[:application_id]}"
   end
